9f41f6914f413ea12c1c85a1278a1667c30c2dc4
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-01-07 13:46:58 -0700

    Add player and goalie class

---
+++ hockey_code.rb
@@ -0,0 +1,24 @@
+### Creat hockey player card game.
+# Create hockey player class.
+# Create hockey goalie class.
+# Create 8 instances of hockey player.
+# Create 2 instances of hockey goalie.
+
+class Hockey_player
+    def initialize(name, speed, wristshot, slapshot, balance, checking, passing, defense)
+        @name = name
+        @speed = speed
+        @wristshot = wristshot
+        @slapshot = slapshot
+        @balance = balance
+        @checking = checking
+        @passing = passing
+        @defense = defense
+    end
+end
+
+class Hockey_goalie
+    def initialize(name, )
+        
+    end
+end
